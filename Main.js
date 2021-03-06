2651bd8b31d560a946e99035ec3e9c3f063d4d02
Shubh-am-15 <shubhamsingh15052001@gmail.com>
2021-03-06 14:44:54 +0530

    Login page html 2.0

---
+++ Main.js
@@ -0,0 +1,4 @@
+function alert1()
+{
+    alert("It is a simple to use platform were you can add, edit and delete text notes. It has a attractive layout and there is search bar to scan and filter your notes.     ");
+}
