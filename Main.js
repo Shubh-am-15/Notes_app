f36beec087b2cedc3cf5ac472a63f02101600e99
Shubh-am-15 <shubhamsingh15052001@gmail.com>
2021-03-06 16:12:09 +0530

    Updating Login page

--- Main.js
+++ Main.js
@@ -1,4 +1,18 @@
-function alert1()
-{
-    alert("It is a simple to use platform were you can add, edit and delete text notes. It has a attractive layout and there is search bar to scan and filter your notes.     ");
-}
+function alert1(){
+    //alert("working");
+      //on clicking button About will be displayed
+     (document.getElementsByClassName("box")).style.display="none";
+     document.getElementsByClassName("About").style.display ="block";
+         
+     
+  }
+  function back1(){
+      alert("working2");
+      //on clicking back button About will again be hidden
+      document.getElementsByClassName("box").style.display="block";
+      document.getElementsByClassName("About").style.display="none";
+  }
+//function alert1()
+//{
+  //  alert("It is a simple to use platform were you can add, edit and delete text notes. It has a attractive layout and there is search bar to scan and filter your notes.     ");
+//}
