9a20f89e0894bbea6807ce86c899ff19a43e4ab7
Shubh-am-15 <shubhamsingh15052001@gmail.com>
2021-03-08 22:16:21 +0530

    added more JS

--- Main2.js
+++ Main2.js
@@ -11,3 +11,24 @@ function show(){
     }
 
 }
+setInterval(displayclock, 700);
+function displayclock()
+{
+    var time = new Date();
+    var hrs= time.getHours();
+    var min =time.getMinutes();
+    var sec = time.getSeconds();
+     if(hrs<10)
+     {
+         hrs= "0"+hrs;
+     }
+     if(min<10)
+     {
+         min= "0"+min;
+     }
+     if(sec<10)
+     {
+         sec= "0"+sec;
+     }
+    document.getElementById("clock").innerHTML= hrs +":"+ min+":"+sec;
+}
